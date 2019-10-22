42c04fd9840c55cf53503d431be9db8662106ba9
Filip734 <f.szymanski98@gmail.com>
2019-10-22 16:29:19 +0200

    removing array

--- client/client.lua
+++ client/client.lua
@@ -10,9 +10,6 @@ local animation = {
 	startTime = 0,
 	progress = 0
 };
-local letters = {
-	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'space'
-};
 
 local userphone = nil;
 
